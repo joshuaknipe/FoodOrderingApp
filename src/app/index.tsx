1dcda75b283f04775360d35a5a87195e5d6248ee
joshknipe <joshuaknipe@gmail.com>
2025-04-16 21:48:37 +0100

    Auth screens (UI)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,19 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link } from 'expo-router';
+import { View, Text } from "react-native";
+import React from "react";
+import Button from "../components/Button";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Link href={"/(user)"} asChild>
         <Button text="User" />
       </Link>
-      <Link href={'/(admin)'} asChild>
+      <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
+      </Link>
+      <Link href={"/sign-in"} asChild>
+        <Button text="Sign In" />
       </Link>
     </View>
   );
